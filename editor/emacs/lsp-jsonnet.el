96faa2495dbf8fa0814de743acb5c47deaa320c1
Christian Schwarzgruber <christian.schwarzgruber@itsv.at>
2023-11-30 19:02:17 +0100

    FREEZE

--- editor/emacs/lsp-jsonnet.el
+++ editor/emacs/lsp-jsonnet.el
@@ -5,27 +5,37 @@
 (require 'jsonnet-mode)
 (require 'lsp-mode)
 
-(defcustom lsp-jsonnet-server-command '("jsonnet-language-server")
-  "Command to start Jsonnet language server."
-  :risky t
+
+(defcustom lsp-jsonnet-executable "jsonnet-language-server"
+  "jsonnet-language-server executable."
   :group 'lsp-jsonnet
-  :type '(repeat string))
+  :type 'string
+  :risky t)
+
+;; TODO(cschwarzgruber):[doc] can be set via env too
+(defcustom lsp-jsonnet-path '()
+  "Additional library search path."
+  :type '(repeat string)
+  :group 'lsp-jsonnet)
 
 ;; Configure lsp-mode language identifiers.
 (add-to-list 'lsp-language-id-configuration '(jsonnet-mode . "jsonnet"))
+
+(lsp-register-custom-settings
+ '(("jsonnet.path" lsp-jsonnet-path)))
 
 ;; Register jsonnet-language-server with the LSP client.
 (lsp-register-client
  (make-lsp-client
   :new-connection (lsp-stdio-connection
                    (lambda ()
-                     lsp-jsonnet-server-command))
+                     (cons lsp-jsonnet-executable
+                           (mapcar (lambda (path) (list "--jpath" path)) (lsp-configuration-section "jsonnet.path")))))
   :activation-fn (lsp-activate-on "jsonnet")
   :initialized-fn (lambda (workspace)
                     (with-lsp-workspace workspace
                       (lsp--set-configuration
-                       ;; TODO: jsonnet-language-server settings should use a prefix
-                       (ht-get (lsp-configuration-section "jsonnet") "jsonnet"))))
+                       (lsp-configuration-section "jsonnet"))))
   :server-id 'jsonnet))
 
 ;; Start the language server whenever jsonnet-mode is used.
